bfb7559dcb002ff17b733f62bb6ee463a5682cd0
Roman Budukh <BudukhRoman@JohnDeere.com>
2023-09-18 17:03:15 +0300

    master
    -add custom MessageResponse
    -add RestDemoExceptionHandler with @ControllerAdvice
    -add RestDemoException - custom exception

--- src/main/java/com/rnb/restDemo/rest/EmployeeRestController.java
+++ src/main/java/com/rnb/restDemo/rest/EmployeeRestController.java
@@ -1,6 +1,7 @@
 package com.rnb.restDemo.rest;
 
 import com.rnb.restDemo.entity.Employee;
+import com.rnb.restDemo.exception.RestDemoException;
 import com.rnb.restDemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,28 +21,36 @@ public class EmployeeRestController {
     }
 
     @GetMapping("/employees")
-    public List<Employee> getEmployees(){
+    public List<Employee> getEmployees() {
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("/employees/{employeeId:\\d+}")
-    public Employee getEmployeesById(@PathVariable("employeeId") int employeeId){
-        return employeeService.findById(employeeId);
+    public Employee getEmployeesById(@PathVariable("employeeId") int employeeId) throws RestDemoException {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RestDemoException("Employee not found for id=%d !!!".formatted(employeeId));
+        }
+        return employee;
     }
 
     @GetMapping("/employees/name/{employeeFirstName}")
-    public List<Employee> getEmployeesByName(@PathVariable("employeeFirstName") String employeeFirstName){
-        return employeeService.findByName(employeeFirstName);
+    public List<Employee> getEmployeesByName(@PathVariable("employeeFirstName") String employeeFirstName) throws RestDemoException {
+        List<Employee> employeeList = employeeService.findByName(employeeFirstName);
+        if (employeeList.isEmpty()) {
+            throw new RestDemoException("Employee not found for name=%s !!!".formatted(employeeFirstName));
+        }
+        return employeeList;
     }
 
     @PostMapping("/employees")
-    public Employee save(@RequestBody Employee employee){
+    public Employee save(@RequestBody Employee employee) {
         Employee saved = employeeService.save(employee);
         return saved;
     }
 
     @PutMapping("/employees")
-    public Employee update(@RequestBody Employee employee){
+    public Employee update(@RequestBody Employee employee) {
         Employee updated = employeeService.update(employee.getId(), employee);
         return updated;
     }
