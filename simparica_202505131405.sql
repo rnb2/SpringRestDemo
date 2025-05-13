dfce18bc35b8c69215d4d67e48771269733a24f3
Roman Budukh <budukh_rn@yahoo.com>
2025-05-13 14:11:01 +0300

    Add Simparica init SQL

---
+++ simparica_202505131405.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.simparica (date_from,date_next,"name","period") VALUES
+	 ('2025-04-11','2025-05-09','Simparica 10-20',28),
+	 ('2025-04-11','2025-05-09','Simparica trio 10-20',28),
+	 ('2025-05-09','2025-06-06','Simparica 10-20',28);
